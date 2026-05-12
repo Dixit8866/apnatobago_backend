b6cf1a22f8db4688242318fb05d42d2dbbe7fea3
Dixit8866 <138421945+Dixit8866@users.noreply.github.com>
2026-05-12 09:49:14 +0530

    commit

--- controllers/delivery/order.controller.js
+++ controllers/delivery/order.controller.js
@@ -513,9 +513,9 @@ export const completeOrderAndSettlePayment = async (req, res) => {
             await user.save({ transaction: t });
         }
 
-        // Ensure current order status is updated to Delivered
+        // Ensure current order status is updated to Payment Collect
         await Order.update(
-            { orderStatus: 'Delivered' }, 
+            { orderStatus: 'Payment Collect' }, 
             { where: { id: assignment.orderId }, transaction: t }
         );
 
@@ -768,7 +768,7 @@ export const settleSingleOrderPayment = async (req, res) => {
                 dueAmount: due,
                 paymentStatus: newPaymentStatus,
                 paymentMethod: finalMethod,
-                orderStatus: 'Delivered',
+                orderStatus: 'Payment Collect',
                 notes: newNotes
             }, { transaction: t });
 
